e09222233eabda3bf730d36c6b04f6281fd0c8e5
Maxwell Norris <maxwellnorris33@gmail.com>
2023-01-06 21:06:45 -0700

    added more changes to card master class

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public abstract class Card : MonoBehaviour
 {
     public int cost;
+    public int cardID; //unique number to distinquish each individual card object
+    public int playerDeck; // which deck does this card belong to
+    public int cardLocation; //is this card in the draw pile, a players hand or the discard 
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,18 @@ public abstract class Card : MonoBehaviour
     void Update()
     {
         
+
     }
 
-    public abstract void ActionPerformed();
+    public abstract void ActionPerformed(); //performs card action and then calls removeFromDeck to put card in discard
+
+    public abstract void cardGrowBig(); //function for card to grow bigger when hovered over
+
+    public abstract void removeFromDeck(); //puts card in discard
+
+    public abstract void addToDeck(); //adds card to deck, changes cardLocation variable.
+
+
+
+
 }
